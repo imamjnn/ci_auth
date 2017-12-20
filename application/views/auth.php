113fc325eb31f24c8d3301f9ed67bed67e5ac95b
Reorocker7 <rerocker7@gmail.com>
2017-12-20 23:17:16 +0700

    add login page

--- application/views/auth.php
+++ application/views/auth.php
@@ -7,40 +7,65 @@
 <body>
 
 	<div ng-controller="AppCtrl">
-		<md-content class="md-padding" layout-xs="column" layout="row" layout-align="center none">		
-			<md-card md-theme="{{ showDarkTheme ? 'dark-orange' : 'default' }}" md-theme-watch>
-				<md-card-title>
-					<span class="md-headline">LOGIN</span>
-				</md-card-title>
-				<md-card-content>
-					<form name="userForm">
-						<md-input-container class="md-block" flex-gt-sm>
-	          				<label>Username</label>
-	          				<input md-maxlength="10" required name="username" ng-model="user.username" />
-				          	<div ng-messages="userForm.username.$error" ng-if="!showHints">
-					            <div ng-message="required">Username is required.</div>
-					            <div ng-message="md-maxlength">The username has to be less than 10 characters long.</div>
-				          	</div>
-	        			</md-input-container>
-	        			<md-input-container class="md-block" flex-gt-sm>
-	        				<label>Password</label>
-	          				<input type="password" required name="password" ng-model="user.password" />
-				          	<div ng-messages="userForm.password.$error" ng-if="!showHints">
-					            <div ng-message="required">Password is required.</div>
-				          	</div>
-	        			</md-input-container>
-	        			<md-input-container class="md-block" flex-gt-sm>
-	        				<label>Confirm Password</label>
-	          				<input type="password" required name="confirm_password" ng-model="user.confirm_password" compare-to="user.password"/>
-				          	<div ng-messages="userForm.confirm_password.$error" ng-if="!showHints">
-					            <div ng-message="required">Password is required.</div>
-					            <div ng-message="compareTo">Must not match</div>
-				          	</div>
-	        			</md-input-container>
-	        		</form>
-				</md-card-content>
+		<div layout="row" layout-align="center none">
+			<md-card flex="30">
+				<md-tabs md-dynamic-height md-border-bottom>
+			      <md-tab label="login">
+			        <md-content class="md-padding">
+			          	<form name="userForm">
+							<md-input-container class="md-block" flex-gt-sm>
+		          				<label>Username</label>
+		          				<input md-maxlength="10" required name="username" ng-model="user.username" />
+					          	<div ng-messages="userForm.username.$error" ng-if="!showHints">
+						            <div ng-message="required">Username is required.</div>
+						            <div ng-message="md-maxlength">The username has to be less than 10 characters long.</div>
+					          	</div>
+		        			</md-input-container>
+		        			<md-input-container class="md-block" flex-gt-sm>
+		        				<label>Password</label>
+		          				<input type="password" required name="password" ng-model="user.password" />
+					          	<div ng-messages="userForm.password.$error" ng-if="!showHints">
+						            <div ng-message="required">Password is required.</div>
+					          	</div>
+		        			</md-input-container>
+		        			<md-button type="submit" class="md-raised">Login</md-button>
+		        		</form>
+			        </md-content>
+			      </md-tab>
+			      <md-tab label="register">
+			        <md-content class="md-padding">
+				        <form name="userForm">
+							<md-input-container class="md-block" flex-gt-sm>
+		          				<label>Username</label>
+		          				<input md-maxlength="10" required name="username" ng-model="user.username" />
+					          	<div ng-messages="userForm.username.$error" ng-if="!showHints">
+						            <div ng-message="required">Username is required.</div>
+						            <div ng-message="md-maxlength">The username has to be less than 10 characters long.</div>
+					          	</div>
+		        			</md-input-container>
+		        			<md-input-container class="md-block" flex-gt-sm>
+		        				<label>Password</label>
+		          				<input type="password" required name="password" ng-model="user.password" />
+					          	<div ng-messages="userForm.password.$error" ng-if="!showHints">
+						            <div ng-message="required">Password is required.</div>
+					          	</div>
+		        			</md-input-container>
+		        			<md-input-container class="md-block" flex-gt-sm>
+		        				<label>Confirm Password</label>
+		          				<input type="password" required name="confirm_password" ng-model="user.confirm_password" compare-to="user.password"/>
+					          	<div ng-messages="userForm.confirm_password.$error" ng-if="!showHints">
+						            <div ng-message="required">Password is required.</div>
+						            <div ng-message="compareTo">Must not match</div>
+					          	</div>
+		        			</md-input-container>
+		        			<md-button type="submit" class="md-raised">Register</md-button>
+		        		</form>
+			        </md-content>
+			      </md-tab>
+			    </md-tabs>
 			</md-card>
-		</md-content>
+		</div>
+
 	</div>
 
   	<?php $this->view('partial/foot') ?>
