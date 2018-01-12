b5a8957653c5c41d7623feeae945b17bffc40ef2
Imam Jinani <imam.jinani@edi-indonesia.local>
2018-01-12 20:39:49 +0700

    Update logout delete session

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -17,34 +17,38 @@ class Auth extends CI_Controller {
 
 	public function login() {
 		$this->load->view('login');
-		$data = $this->session->userdata();
-		if($data){
-			echo 'belum login';
-		}else{
-			echo 'sudah login';
-		}
-		deb($data);
+		// $data = $this->session->userdata('token');
+		// if($data){
+		// 	echo 'sudah login';
+		// }else{
+		// 	echo 'belum login';
+		// }
+		
 	}
 
 	public function logout() {
-		$data = $this->session->userdata();
-		deb($data);
+		$data = $this->session->userdata('token');
 		if($data){
 			$this->USession->removeByCond(['token'=>$data['token']]);
 			$this->session->sess_destroy();
+			$result['status'] = 'success';
+			$result['message'] = 'Logout success.';
 		}else{
-			echo json_encode('ente belum login sudah logout aja');
+			$result['status'] = 'failed';
+			$result['message'] = 'Error, maybe you are not logged in.';
 		}
+		echo json_encode($result);
 	}
 
 	public function check_login() {
 		$decod = json_decode(file_get_contents("php://input"));
-		// $username = $decod->username;
-		// $password =	$decod->password;
+		$username = $decod->username;
+		$password =	$decod->password;
 
-		$username = $this->input->post('username');
-		$password =	$this->input->post('password');
+		// $username = $this->input->post('username');
+		// $password =	$this->input->post('password');
 
+		// get ip client
 		$ip = $_SERVER['REMOTE_ADDR']?:($_SERVER['HTTP_X_FORWARDED_FOR']?:$_SERVER['HTTP_CLIENT_IP']);
 
 		// encryp password
@@ -56,7 +60,9 @@ class Auth extends CI_Controller {
 		$check_user = $this->User->checkUsername($username);
 
 		if($check_user == true && $check_pass == true){
+			// create token
 			$token = password_hash($user->username, PASSWORD_BCRYPT, ['cost'=>10]);
+			
 			$sess_data = array('id'=>$user->id, 'username'=>$user->username, 'token'=>$token);
 			$sess_key = $this->session->set_userdata($sess_data);			
 			$sess_save = $this->USession->create(['user'=>$user->id, 'token' => $token, 'ip'=>$ip]);
--- application/views/login.php
+++ application/views/login.php
@@ -15,8 +15,11 @@
             			<h2><span>Login Auth</span></h2>
           			</div>
 				</md-toolbar>
+				<div layout="row" layout-sm="column" layout-align="space-around">
+      				<md-progress-circular ng-show="isLoading" md-mode="indeterminate"></md-progress-circular>
+    			</div>
 				<md-card-content>
-					<form name="FormLogin" novalidate>
+					<form name="FormLogin" ng-submit="onSubmit()" novalidate>
 						<md-input-container class="md-block" flex-gt-sm>
 	          				<label>Username</label>
 	          				<input type="text" required name="username" ng-model="user.username"/>
@@ -31,7 +34,9 @@
 					            <div ng-message="required">Please enter your password.</div>
 				          	</div>
 	        			</md-input-container>
-	        			<md-button ng-disabled="!FormLogin.$valid" type="submit" class="md-raised md-primary">Login</md-button>
+	        			<md-button ng-disabled="!FormLogin.$valid" type="submit" class="md-raised md-primary">
+	        				Login
+	        			</md-button>
 					</form>
 				</md-card-content>
 			</md-card>
@@ -44,13 +49,44 @@
   	<script>
     	var app = angular.module('authPage', ['ngMaterial', 'ngMessages']);
 
-		app.controller('AppCtrl', function($scope) {
+		app.controller('AppCtrl', function($scope, $http, $location, $mdToast) {
 			$scope.showHints = false;
 		  	$scope.isDisabled = true;
+		  	$scope.isLoading = false;
 		  	$scope.user = {
 	      		username: "",
 	      		password: ""
     		};
+
+    		$scope.onSubmit = function(){
+    			$scope.isLoading = true;
+    			var login = {
+	    			method: 'POST',
+	    			url: 'http://localhost:4545/ci_auth/auth/check_login',
+	    			headers: {
+	    				'Content-Type': 'application/x-www-form-urlencoded'
+	    			},
+	    			data: {
+	    				'username': $scope.user.username,
+	    				'password': $scope.user.password
+	    			}
+	    		};
+	    		$http(login).then(function(res){
+	    			$scope.isLoading = false;
+	    			if(res.data.status === 'success'){
+	    				console.log('login berhasil, selanjutnya terserah anda');
+	    			}else{
+	    				$mdToast.show(
+      						$mdToast.simple()
+					        .textContent(res.data.message)
+					        .position('top')
+					        .hideDelay(3000)
+					    );
+	    			}
+	    		});
+    		}
+
+    		
 		})
 		.config(function($mdThemingProvider){
 			$mdThemingProvider.theme('dark-orange').backgroundPalette('orange').dark();
